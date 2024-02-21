e0ca709813ae20561cee76a23985c525b5618185
Luke Braithwaite <lukebraithwaite7@gmail.com>
2024-02-21 18:35:21 +0000

    :sparkles: added code naive explainer skeleton

---
+++ gnn4ua/naive_explainer.py
@@ -0,0 +1,13 @@
+def brute_force_explainer(neg_concepts, examples):
+    """
+    Brute force explaination function to find minimal omitted lattice
+    :return:
+    """
+
+    # 1) Get most negative weighted graph concepts which are lattices from GNN
+
+    # 2) Check all false examples in the dataset if any are sublattices from these
+    #    graph concepts
+
+    # 3) Check if this lattice is omitted by all of the true lattices
+    ...
