2e1a101f978ddd79bffc6340cb985448109f4a1e
Luke Braithwaite <lukebraithwaite7@gmail.com>
2024-02-29 16:20:47 +0000

    :sparkles: initial version of feature extractor to run

--- gnn4ua/glg_explainer.py
+++ gnn4ua/glg_explainer.py
@@ -8,10 +8,11 @@ from torch_geometric.explain.config import (
     ModelTaskLevel,
     ModelMode,
     ModelReturnType,
-    MaskType, ThresholdConfig,
-    ThresholdType
+    MaskType,
+    ThresholdType, ThresholdConfig,
 )
 from torch_geometric.loader import DataLoader
+from torch_geometric.utils import to_undirected
 
 from gnn4ua.datasets.loader import (LatticeDataset, Targets,
                                     GeneralisationModes, )
@@ -38,26 +39,27 @@ def generate_motifs(model: nn.Module, train_data, test_data):
         explanation_type=ExplanationType.phenomenon,
         model_config=config,
         edge_mask_type=MaskType.object,
-        # threshold_config=ThresholdConfig(
-        #     threshold_type=ThresholdType.hard,
-        #     value=0.
-        # )
+        threshold_config=ThresholdConfig(
+            threshold_type=ThresholdType.topk_hard,
+            value=16
+        )
     )
 
     assert isinstance(explainer.algorithm, PGExplainer)
 
+    for train_sample in DataLoader(train_data, batch_size=1, shuffle=True):
+        for epoch in range(10):
+            explainer.algorithm.train(
+                epoch,
+                model,
+                train_sample.x,
+                train_sample.edge_index,
+                target=train_sample.y,
+                index=0,
+                batch=train_sample.batch
+            )
+
     test_sample = next(iter(DataLoader(test_data, batch_size=1, shuffle=False)))
-    # for train_sample in DataLoader(train_data, batch_size=1):
-    for epoch in range(10):
-        explainer.algorithm.train(
-            epoch,
-            model,
-            test_sample.x,
-            test_sample.edge_index,
-            target=test_sample.y,
-            index=0,
-            batch=test_sample.batch
-        )
     return explainer(test_sample.x, test_sample.edge_index, target=test_sample.y,
                      batch=test_sample.batch,
                      index=0)
@@ -81,11 +83,16 @@ def main():
         '../experiments/results/task_Distributive/models/BlackBoxGNN_generalization_strong_seed_102_temperature_1_embsize_16.pt'))
 
     motifs = generate_motifs(gnn, train_data, test_data)
-    print(motifs)
-    motifs.visualize_graph()
     print(motifs.edge_mask)
 
-    # motifs.visualize_graph()
+    new_edge_index, new_edge_mask = to_undirected(edge_index=motifs.edge_index,
+                                                  edge_attr=motifs.edge_mask,
+                                                  reduce='mean')
+    print(new_edge_mask)
+    motifs.update({'edge_index': new_edge_index, 'edge_mask': new_edge_mask})
+    motifs = motifs.threshold(threshold_type=ThresholdType.hard, value=0.1)
+    print(motifs.edge_mask)
+    motifs.visualize_graph()
 
 
 if __name__ == "__main__":
