d51a867b8da589a24262da8b1c5c293f427c23f3
Luke Braithwaite <lukebraithwaite7@gmail.com>
2024-03-02 13:40:57 +0000

    :sparkles: reworked cli to have appropriate help messages

---
+++ gnn4ua/run_glgexplainer.py
@@ -0,0 +1,5 @@
+from gnn4ua.datasets.loader import Targets, GeneralisationModes
+
+
+def run_glgexplainer(task: Targets, generalisation_modes: GeneralisationModes):
+    ...
--- main.py
+++ main.py
@@ -4,19 +4,29 @@ from gnn4ua.datasets.loader import GeneralisationModes, Targets
 from gnn4ua.local_feature_extractor import generate_local_motif
 
 
-@click.group()
+@click.group(name="GLGExplainer for Universal Algebras")
 def cli():
     ...
 
 
-@cli.command()
-@click.option('--task',
-              type=click.Choice(['Distributive', 'Modular', 'Meet_SemiDistributive',
-                                 'Join_SemiDistributive', 'multilabel']),
-              default='Distributive'
+@cli.command(
+    help="Extracts local motifs from the classifier to be used as a basis for explainations"
+)
+@click.option(
+    '--task',
+    type=click.Choice(
+        ['Distributive', 'Modular', 'Meet_SemiDistributive', 'Join_SemiDistributive',
+         'multilabel']
+    ),
+    default='Distributive',
+    help='Task to extract motifs for'
               )
-@click.option('--generalisation_mode', type=click.Choice(['weak', 'strong']),
-              default='strong')
+@click.option(
+    '--generalisation_mode',
+    type=click.Choice(['weak', 'strong']),
+    default='strong',
+    help='Generalisation mode used to train the GNN'
+)
 def extract_motifs(task: str, generalisation_mode: str):
     task = Targets[task]
     generalisation_mode = GeneralisationModes[generalisation_mode]
