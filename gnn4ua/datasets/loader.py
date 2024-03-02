f2cd0dfe0da740c47f79099fba182271d16741c4
Luke Braithwaite <lukebraithwaite7@gmail.com>
2024-03-02 18:24:54 +0000

    :sparkles: fixed loader

--- gnn4ua/datasets/loader.py
+++ gnn4ua/datasets/loader.py
@@ -80,7 +80,7 @@ class LatticeDataset(InMemoryDataset):
             y = torch.LongTensor([row[label_names].to_list()])
         else:
             label = row[self.target]
-            y = torch.LongTensor([1 - label, label])
+            y = torch.unsqueeze(torch.LongTensor([1 - label, label]), 0)
         return Data(x=x, edge_index=edge_index, y=y)
 
     def process(self):
