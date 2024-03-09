54ece3f3029ee696a076647fd7193343e5cdf5e0
Luke Braithwaite <lukebraithwaite7@gmail.com>
2024-03-09 21:49:15 +0000

    :see_no_evil: updated .gitignore

--- extract_motifs.sh
+++ extract_motifs.sh
@@ -7,7 +7,7 @@ seeds=( '102' '106' '270' )
 for task in "${tasks[@]}"; do
   for seed in "${seeds[@]}"; do
       for mode in "${modes[@]}"; do
-        python main.py extract-motifs --task="$task" --generalisation_mode="$mode" --n_epochs=10 --seed="$seed"
+        python main.py extract-motifs --task="$task" --generalisation_mode="$mode" --n_epochs=100 --seed="$seed"
       done
   done
 done
